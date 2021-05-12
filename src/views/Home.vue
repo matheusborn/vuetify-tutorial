0b5df6458104fec9ab4495f0c164ace4b8c82ac2
Matheus Barros <matheusborn@hotmail.com>
2021-05-12 11:38:11 -0300

    Text & Colors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="home">
-    <h1>This is an home page</h1>
+    <h1>Home Page</h1>
+    <p class="red white--text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+      expedita, illo, earum esse nulla quisquam nisi a inventore asperiores
+      quidem vero, doloribus eum rerum eaque accusamus omnis neque corrupti.
+      Esse.
+    </p>
+    <p class="pink lighten-4 red--text text--darken-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui repudiandae,
+      commodi voluptas quidem quis libero cum, sunt eaque mollitia nulla autem
+      quas quos dolore quae similique aperiam aut soluta obcaecati?
+    </p>
+    <h1 class="display-4 text-lowercase">Massive Display</h1>
+    <h4 class="display-1">Small display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading">This is a subheading</p>
+    <p class="caption font-weight-bold">This is a caption</p>
   </div>
 </template>
 
