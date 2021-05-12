ca082762fe5e492aeb5bbfc6d4c5e8d9b94fe3aa
Matheus Barros <matheusborn@hotmail.com>
2021-05-12 16:15:05 -0300

    Toolbars

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>
+    <v-app-bar flat app>
+      <v-app-bar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Todo</span>
+        <span>Ninja</span>
+      </v-app-bar-title>
+      <v-spacer></v-spacer>
+      <v-btn plain color="grey">
+        <span>Sign Out</span>
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.text {
+  white-space: normal;
+}
+</style>
